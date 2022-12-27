34c086538e755db9ce07e3b6e353b37966046cce
lkdcode <dlrlejr1@naver.com>
2022-12-28 02:12:21 +0900

    ✨ 메인 어플리케이션

---
+++ src/main/java/study/TravelClubApplication.java
@@ -0,0 +1,15 @@
+package study;
+
+public class TravelClubApplication {
+    private static UserInformation userInformation;
+
+    public static void main(String[] args) {
+
+//        userInformation = new UserInformation();
+//        System.out.println(userInformation.getUserIdList());
+
+        String test = "asfg!@#!@#";
+        System.out.println(test.toUpperCase().equals(test));
+        System.out.println(test.toLowerCase().equals(test));
+    }
+}
